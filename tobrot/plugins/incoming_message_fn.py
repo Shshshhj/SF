9120c1ac7038e4a14090150dbac92c8db7f5f913
Jzjssje <58160780+Shshshhj@users.noreply.github.com>
2021-05-12 21:56:26 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -53,9 +53,6 @@ async def incoming_message_f(client, message):
     """/leech command or /gleech command"""
     user_command = message.command[0]
     g_id = message.from_user.id
-    credit = await message.reply_text(
-        f"🧲 Leeching for you <a href='tg://user?id={g_id}'>🤕</a>", parse_mode="html"
-    )
     i_m_sefg = await message.reply_text("processing...", quote=True)
     # get link from the incoming message
     if message.reply_to_message:
